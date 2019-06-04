5261240b7e95eb4d1daf2c9b90d4dd795516cbdd
nikolaimar2019 <doktora_g@abv.bg>
2019-06-04 16:36:38 +0300

    asd

---
+++ IRunes.Data/RunesDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace IRunes.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class RunesDbContext : DbContext
+    {
+        public RunesDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
